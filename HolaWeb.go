8d1eb114e29046cb6d1bc0bcd1870f1ca1d67edc
Carlos Andrés Aguirre Cañas <Carlos Andrs Aguirre Caas>
2017-03-18 20:34:35 -0500

    Se crea una funcion de prueba HandleFunc

--- HolaWeb.go
+++ HolaWeb.go
@@ -11,7 +11,15 @@ type Usuario struct {
 	Contraseña string
 }
 
+func mostrarUsuario(w http.ResponseWriter, r *http.Request) {
+	usuario1 := Usuario{"usuario", "usuario1"}
+	json.NewDecoder(r)
+	json.NewEncoder(w).Encode(usuario1)
+}
+
 func main() {
+	http.HandleFunc("/usuario", mostrarUsuario)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, arg2 *http.Request) {
 		usuarioAndres := Usuario{"andrés", "contraseña123"}
 		json.NewEncoder(w).Encode(usuarioAndres)
