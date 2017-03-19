181ff1d12fb74a360a848506f46b7c2939dda0ba
Carlos Andrés Aguirre Cañas <Carlos Andrs Aguirre Caas>
2017-03-19 10:21:22 -0500

    Se identifica el usuario actual implementando el paquete os/user.

--- HolaWeb.go
+++ HolaWeb.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os/user"
 )
 
 // Usuario de la pagina.
@@ -12,17 +15,28 @@ type Usuario struct {
 }
 
 func mostrarUsuario(w http.ResponseWriter, r *http.Request) {
-	usuario1 := Usuario{"usuario", "usuario1"}
-	json.NewDecoder(r)
+
+	usuario1 := Usuario{"user", "000"}
+	//json.NewDecoder(r)
 	json.NewEncoder(w).Encode(usuario1)
 }
 
 func main() {
+	u, err := user.Current()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	usuarioAndres := Usuario{u.Name, u.Username}
+	fmt.Println("Usuario actual:")
+	fmt.Println(usuarioAndres.Nombre)
+
 	http.HandleFunc("/usuario", mostrarUsuario)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, arg2 *http.Request) {
-		usuarioAndres := Usuario{"andrés", "contraseña123"}
+		//usuarioAndres := Usuario{"andrés", "contraseña123"}
 		json.NewEncoder(w).Encode(usuarioAndres)
 	})
 	http.ListenAndServe(":8002", nil)
+
 }
